39843b233e2db9729886556c0c6fda31e1e09995
roxane-vnldn <roxane.vanolden@laposte.net>
2024-12-12 16:31:28 +0000

    Adding the roxygen documentation for summary.Studentdata.R

--- R/summary.StudentData.R
+++ R/summary.StudentData.R
@@ -1,3 +1,26 @@
+#' Summarize students performances
+#'
+#'This function computes summary statistics (e.g., mean, min, max) for student performance metrics (math.score, writing.score, and reading.score) grouped by specified categorical variables in a dataset.
+#'
+#' @param obj A dataset of class \code{StudentData} containing
+#'  at least the variables \code{math.score}, \code{writing.score}, and \code{reading.score}.
+#'  Other variables may be used as grouping variables.
+#' @param group_var A character vector specifying the column name(s) to group the data by.
+#' @param summary_func A function to summarize each numeric variable
+#'  (e.g., \code{mean}, \code{min}, or \code{max}). The function is applied to
+#'  \code{math.score}, \code{writing.score}, and \code{reading.score} within each group
+#'  defined by \code{group_var}. Defaults to \code{mean}
+#'
+#' @return A \code{tibble} containing the grouped data and the summarized statistics for
+#'  \code{math.score}, \code{writing.score}, and \code{reading.score}.
+#' @importFrom dplyr "filter" "group_by" "summarise_all" "all_of" "across"
+#' @importFrom tibble "as_tibble"
+#' @export
+#'
+#' @examples
+#' data(students)
+#' data <- prepare_student_data(students, scale = FALSE)
+#' summary(obj = data, group_var = c("gender", "lunch"), summary_func = median)
 summary.StudentData <- function(obj,
                                 group_var,
                                 summary_func = mean) {
